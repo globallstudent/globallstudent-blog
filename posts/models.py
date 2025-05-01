7facddf86461f934cb80f0ec0d3a71e9dc842694
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-05-01 16:39:00 +0500

    modify app

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=1000, null=True, blank=True)
--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from posts.views import PostListView
+from posts.views import PostListAPIView, PostDetailAPIView, PostCreateAPIView
 
 urlpatterns = [
-    path('all', PostListView.as_view())
+    path('all/', PostListAPIView.as_view()),
+    path('<int:pk>/', PostDetailAPIView.as_view()),
+    path('create/', PostCreateAPIView.as_view())
 ]
