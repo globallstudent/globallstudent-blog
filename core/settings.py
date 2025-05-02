a232da5f8d16a914f47c5e253cb98344f8758f2c
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2025-05-02 07:58:27 +0500

    app updates

--- core/settings.py
+++ core/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = 
+
+STATICFILES_DIR = [ BASE_DIR / "static" ]
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from posts.views import PostListAPIView, PostDetailAPIView, PostCreateAPIView
+from posts.apis import PostCreateAPIView, PostListAPIView, PostDetailAPIView
+from posts import views
 
 urlpatterns = [
     path('all/', PostListAPIView.as_view()),
     path('<int:pk>/', PostDetailAPIView.as_view()),
-    path('create/', PostCreateAPIView.as_view())
+    path('create/', PostCreateAPIView.as_view()),
+
+    path('hello/', views.hello_world, name="hello_world")
 ]
